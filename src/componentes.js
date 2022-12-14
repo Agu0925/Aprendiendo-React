ff9dc813e9dd4a57d69ada85ba845d1dc6cc6ad2
Agustin A <agustin.ahlers@hotmail.com>
2022-12-14 03:09:43 -0300

    first

---
+++ src/componentes.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useState } from 'react';
+//Create a new component
+const App = () => {
+  return (
+    <h1 id="title">Component with react</h1>
+  );
+};
+const App2 = () => {
+  return (
+    <>
+      <div className="redes">
+        <p>G</p>
+        <p>L</p>
+        <p>I</p>
+      </div>
+      <div className="redes2">
+        <p>G</p>
+        <p>L</p>
+        <p>I</p>
+      </div>
+    </>
+  );
+};
+export const App3 = () => {
+  return (
+    <>
+      <App />
+      <App2 />
+      <div id="container">
+        <div id="medio">
+          <h2>Mid</h2>
+          <h2>Mid</h2>
+          <h2>Mid</h2>
+          <h2>Mid</h2>
+          <h2>Mid</h2>
+          <h2>Mid</h2>
+        </div>
+        <div id="mFlex">
+          <h2 className="mf">Mid Flex</h2>
+          <h2 className="mf">Mid Flex</h2>
+          <h2 className="mf">Mid Flex</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+export const App4 = () => {
+    const [value, setValue] = useState("");
+    const [value2, setValue2] = useState("");
+    const [value3, setValue3] = useState("");
+    return (
+      <>
+        <input id='nombre' type="text" value={value} onChange={e => setValue(e.target.value)} placeholder='Nombre'></input>
+        <input id='apellido' type="text" value={value2} onChange={e => setValue2(e.target.value)} placeholder='Nombre'></input>
+        <input id='mail' type="text" value={value3} onChange={e => setValue3(e.target.value)} placeholder='Nombre'></input>
+        <button onClick={() => {
+          localStorage.setItem("nombre", value);
+          localStorage.setItem("apellido", value2);
+          localStorage.setItem("mail", value3);
+        }}>
+          Guardar valor en el almacenamiento local
+        </button>
+      </>
+    )
+};
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from "react-dom";
+import "./App.css";
+import {App3} from './componentes';
+import {App4} from './componentes';
+const AppReact = () => {
+    return (
+        <>
+            <App3 />
+            <div className='center'>
+                <h2>Guardar en localStorage con ReactJS</h2>
+                <App4 />
+            </div>
+        </>
+    );
+};
+//Show the component on the element with id "root" in the HTML
+ReactDOM.render(<AppReact />, document.getElementById("root"));
+if (localStorage.getItem("react")){
+    document.getElementById("nombre").value = localStorage.getItem("nombre");
+    document.getElementById("apellido").value = localStorage.getItem("apellido");
+    document.getElementById("mail").value = localStorage.getItem("mail");
+}
